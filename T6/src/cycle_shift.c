6b4489e3aa4cb2f0b9a3cb4caa1d1723756aa0bf
Ivan Bogomolov <qhomop@gmail.com>
2023-10-31 20:58:18 +0300

    Update cycle_shift.c

--- T6/src/cycle_shift.c
+++ T6/src/cycle_shift.c
@@ -1,74 +1,69 @@
 #include <stdio.h>
 
-#define customAbs(x) ((x < 0) ? -(x) : (x))
+#define NMAX 10
+#define CUSTOM_ABS(x) ((x < 0) ? -(x) : (x))
 
-int input(int *a, int arr_size, int *shift);
-void output(int *a, int arr_size);
-void cyclicShiftLeft(int *arr, int n, int shift);
-void cyclicShiftRight(int *arr, int n, int shift);
+void input (int *buffer, int *length, int *shift);
+void output (int *buffer, int length);
+void cyclicShift(int *buffer, int length, int shift);
 
-int main(void){
-    int arr_size;
-    int shift;
-    char garbage;
-    if(scanf("%d%c", &arr_size, &garbage) == 2 && garbage == '\n'){
-        int array[arr_size];
-        if(input(array, arr_size, &shift) == 1){
-        printf("n/a");
-        return 1;
-    }
+int main(){
 
-    (shift >= 0) ? cyclicShiftLeft(array, arr_size, shift) : cyclicShiftRight(array, arr_size, shift);
+    int buffer[NMAX];
+	int length, shift;
 
-    output(array, arr_size);
+	input(buffer, &length, &shift);
 
-    return 0;
+	if(length > NMAX || length <= 0){
+		printf("n/a");
+		return 1;
+	}
 
-    } else{
-        printf("n/a");
-        return 1;
-    }
+    cyclicShift(buffer, length, shift);
+
+    output(buffer, length);
+
 }
 
-int input(int *a, int arr_size, int *shift){
-    char garbage;
-    for(int *p = a; p - a < arr_size; p++){
-        if(scanf("%d%c", p, &garbage) == 2 && garbage == '\n'){} else {
-            return 1;
+void input(int *buffer, int *lenght, int *shift){
+	scanf("%d", lenght);
+	if(*lenght > NMAX || *lenght <= 0){
+		return;
+	}
+	for(int *p = buffer; p - buffer < *lenght; ++p){
+		scanf("%d", p);
+	}
+
+    scanf("%d", shift);
+}
+
+void output(int *buffer, int lenght){
+	for(int *p = buffer; p - buffer < lenght; ++p){
+		printf("%d", *p);
+		if(p - buffer < lenght - 1){
+			printf(" ");
+		}
+	}
+}
+
+void cyclicShift(int *buffer, int length, int shift){
+    int temp;
+    shift %= length;
+    if(shift > 0){
+        for(int i = 0; i < shift; ++i){
+            temp = buffer[0];
+            for(int j = 0; j < length - 1; j++){
+                buffer[j] = buffer[j+1];
+            }
+            buffer[length - 1] = temp;
         }
-    }
-
-    if(scanf("%d%c", shift, &garbage) == 2 && garbage == '\n'){} else {
-            return 1;
-    }
-
-    return 0;
-}
-
-void output(int *a, int arr_size){
-    for(int *p = a; p - a < arr_size; p++){
-        printf("%d ", *p);
-    }
-}
-
-void cyclicShiftLeft(int *arr, int n, int shift) {
-    shift = shift % n;
-    for (int i = 0; i < shift; i++) {
-        int temp = arr[0];
-        for (int j = 0; j < n - 1; j++) {
-            arr[j] = arr[j + 1];
+    } else if(shift < 0){
+        for(int i = 0; i < CUSTOM_ABS(shift); i++){
+            temp = buffer[length - 1];
+            for (int j = length - 1; j > 0; --j){
+                buffer[j] = buffer[j - 1];
+            }
+            buffer[0] = temp;
         }
-        arr[n - 1] = temp;
-    }
-}
-
-void cyclicShiftRight(int *arr, int n, int shift){
-    shift = shift % n;
-    for (int i = 0; i < customAbs(shift); i++){
-        int temp = arr[n-1];
-        for (int j = n-1; j > 0; j--) {
-            arr[j] = arr[j - 1];
-        }
-        arr[0] = temp;
     }
 }
