e50e1a725a49f28dd18a1368d3af686fd9da27ef
Ivan Bogomolov <qhomop@gmail.com>
2023-11-02 15:02:20 +0300

    Update sort.c

--- T7/src/sort.c
+++ T7/src/sort.c
@@ -2,78 +2,79 @@
 #include <stdlib.h>
 
 #define ARR_SIZE 10
-#define OK 0
-#define ERR 1
+#define OK 1
+#define ERR 0
 
 int allocate(int **m, int *n);
 void output(int *a);
 void swap(int *a, int *b);
 void quickSort(int *arr, int j);
 
-int main(){
+int main() {
     int *m = NULL;
-    int n = 0;
-    if(allocate(&m, &n) == 1){
+    int n;
+
+    if (allocate(&m, &n) == 0) {
         printf("n/a");
         return 1;
     }
-    quickSort(m, n-1);
+    quickSort(m, ARR_SIZE - 1);
     output(m);
 
     return 0;
 }
 
-int allocate(int **m , int *n){
-    int status = OK;
-    if(scanf("%d", n) == 1 && *n > 0){
-        *m = (int*)malloc((*n) * sizeof(int));
-        if(m == NULL){
-            status = ERR;
-        }
-
-        char garbage;
-        for(int i = 0; i < *n; i++){
-            if(scanf("%d%c", &(*m)[i], &garbage) == 2 && garbage == '\n'){
-                status = OK;
-            } else {
-                free(*m);
-                status = ERR;
-                *m = NULL;
+int allocate(int **m, int *n) {
+    int flag = OK;
+    if (scanf("%d", n) == 1 && *n > 0) {
+        *m = (int *)malloc(*n * sizeof(int));
+        if (!m) {
+            flag = ERR;
+        } else {
+            for (int i = 0; i < *n; i++) {
+                if (scanf("%d", &(*m)[i]) != 1) {
+                    flag = ERR;
+                    *m = NULL;
+                }
             }
         }
+        free(*m);
     }
 
-    return status;
+    return flag;
 }
 
-void output(int *a){
-    for(int *p = a; p - a < ARR_SIZE; p++){
-        printf("%d ", *p);
+void output(int *a) {
+    for (int *p = a; p - a < ARR_SIZE; p++) {
+        printf("%d", *p);
+        if (p - a < ARR_SIZE - 1) {
+            printf(" ");
+        }
     }
 }
 
-void swap(int *a, int *b){
+void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void quickSort(int *array, int right_iterator){
+void quickSort(int *array, int right_iterator) {
     int arr_size = right_iterator;
     int left_iterator = 0;
-    int pivot = array[(int)(right_iterator/2)];
+    int pivot = array[(int)(right_iterator / 2)];
     do {
-        while(array[left_iterator] < pivot) left_iterator++;
-        while(array[right_iterator] > pivot) right_iterator--;
+        while (array[left_iterator] < pivot) left_iterator++;
+        while (array[right_iterator] > pivot) right_iterator--;
 
-        if (left_iterator <= right_iterator){
+        if (left_iterator <= right_iterator) {
             swap(&array[left_iterator], &array[right_iterator]);
             left_iterator++;
             right_iterator--;
         }
 
-    } while(left_iterator <= right_iterator);
+    } while (left_iterator <= right_iterator);
 
     if (right_iterator > 0) quickSort(array, right_iterator);
-    if (arr_size > left_iterator) quickSort(array+left_iterator, arr_size-left_iterator);
+    if (arr_size > left_iterator) quickSort(array + left_iterator, arr_size - left_iterator);
 }
